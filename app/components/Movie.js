4f717c3388aba44b9d608f1212b55336157bf895
Nacho Martín <nacho@limenius.com>
2018-09-12 20:24:49 +0200

    navigation

--- app/components/Movie.js
+++ app/components/Movie.js
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import { movies, actors } from "../data";
 import MovieHeader from "./MovieHeader";
 import ListItem from "./ListItem";
-
-import { View, FlatList } from "react-native";
+import Colors from "../styles/colors";
+import { View, TouchableHighlight, FlatList } from "react-native";
 
 export default class Movie extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.state.params.name,
+  });
+
   constructor(props) {
     super(props);
     this.state = {
-      movie: movies.find(movie => movie.name === "Pulp Fiction"),
+      movie: movies.find(
+        movie => movie.name === props.navigation.state.params.name,
+      ),
       actors: actors,
     };
   }
 
   render() {
+    const { movie } = this.state;
+    const { navigate } = this.props.navigation;
+
     return (
       <View>
         <FlatList
-          data={this.state.movie.actors}
-          ListHeaderComponent={<MovieHeader movie={this.state.movie} />}
+          data={movie.actors}
+          ListHeaderComponent={<MovieHeader movie={movie} />}
           keyExtractor={item => item}
           renderItem={({ item }) => (
             <ListItem name={item} image={this.state.actors[item].image} />
--- app/components/MovieList.js
+++ app/components/MovieList.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import { movies, actors } from "../data";
 import MainHeader from "./MainHeader";
 import ListItem from "./ListItem";
-import { View, FlatList } from "react-native";
+import Colors from "../styles/colors";
+
+import { View, TouchableHighlight, FlatList } from "react-native";
 
 export default class MovieList extends Component {
+  //static navigationOptions = {
+  //  title: "Movies",
+  //};
+
   constructor(props) {
     super(props);
     this.state = {
@@ -13,12 +19,26 @@ export default class MovieList extends Component {
   }
 
   render() {
+    const { navigate } = this.props.navigation;
+
     return (
       <View>
-        <MainHeader />
-        {this.state.movies.map(movie => (
-          <ListItem key={movie.name} name={movie.name} image={movie.image} />
-        ))}
+        <FlatList
+          data={this.state.movies}
+          ListHeaderComponent={<MainHeader />}
+          keyExtractor={item => item.name}
+          renderItem={({ item }) => (
+            <TouchableHighlight
+              underlayColor={Colors.subtleAccent}
+              activeOpacity={0.5}
+              onPress={() => navigate("Movie", { name: item.name })}
+            >
+              <View>
+                <ListItem name={item.name} image={item.image} />
+              </View>
+            </TouchableHighlight>
+          )}
+        />
       </View>
     );
   }
